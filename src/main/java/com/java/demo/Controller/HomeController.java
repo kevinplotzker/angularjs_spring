7859e8f4f432dd404f2443c1d87207f1a07c369a
Kevin Plotzker <kevinplotzker@gmail.com>
2019-09-25 18:24:33 -0700

    Frontend changes

---
+++ src/main/java/com/java/demo/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.java.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home() {
+        return "/view/index.html";
+    }
+}
